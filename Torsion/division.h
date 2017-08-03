d6994447df73412c6c107799fedd084830a45fa0
Romanovg185 <romanovangenderen@gmail.com>
2017-08-03 20:19:29 +0200

    Delete division.h

--- Torsion/division.h
+++
@@ -1,9 +0,0 @@
-#ifndef DIVISION_H_INCLUDED
-#define DIVISION_H_INCLUDED
-
-std::pair<Coordinate, bool> equidistantPointOnLine(Coordinate p1, Coordinate p2, Coordinate circleCenter, double radius);
-void correctHead(std::vector<Coordinate> &pos, double D);
-void relax(std::vector<Coordinate> &myArray);
-void divide(Particle &pOld, Particle &pNew);
-
-#endif // DIVISION_H_INCLUDED
